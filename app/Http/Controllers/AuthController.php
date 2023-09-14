035a166d7ba9efa86a56a9ee625cb78b341c3b65
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-09-14 10:50:46 +0200

    ....................

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
 class AuthController extends Controller
 {
     public function create(Request $request){
